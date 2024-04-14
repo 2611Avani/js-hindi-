fb7bdc8d9907e616ddeb59a55a27af3fe94e635e
2611Avani <166926965+2611Avani@users.noreply.github.com>
2024-04-14 14:53:34 +0000

    container config done

---
+++ .devcontainer/01_baiscs /01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "avani@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 - not allowed as it is constant 
+accountEmail = "a@gmail.com"
+accountPassword = "00000"
+accountCity = "Indore"
+
+
+console.log(accountId);
+console.table([accountId, accountPassword, accountCity, accountEmail, accountState]);
