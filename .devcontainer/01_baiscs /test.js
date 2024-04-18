88b3d30fc3403bd4cfeb7946a43ffc787c9a46f0
2611Avani <166926965+2611Avani@users.noreply.github.com>
2024-04-18 12:53:25 +0000

    test

--- .devcontainer/01_baiscs /test.js
+++ .devcontainer/01_baiscs /test.js
@@ -1,1 +1,47 @@
-console.log("Avani")
+// let accountId = 123
+// let accountemail= "abc@gmail.com"
+// let accountPassword = "0000"
+// let accountCity = "indore"
+// let accountState;
+
+// console.table([accountId, accountemail, accountPassword, accountCity, accountState]);
+
+// accountId= 231
+// accountemail = "a@g.com"
+// accountPassword = 1111n
+// accountCity= null
+// accountState;
+
+// console.table([accountId, accountemail, accountPassword, accountCity, accountState]); 
+
+// console.table([(typeof accountId), (typeof accountemail), (typeof accountPassword), (typeof accountCity), (typeof accountState)]);
+
+
+// let score = 123
+// let newScore = Number (score)
+
+// console.log(typeof score);
+// console.log(typeof newScore);
+// console.log(newScore);
+
+// let a = 3
+// nega = -a
+//console.log(nega);
+
+//console.log(2+2);
+
+// let c = "hi"
+// let b = " everyone"
+// console.log(c+b);
+
+
+
+
+
+
+
+
+
+
+
+
