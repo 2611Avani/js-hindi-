924ee3b4bcc4ee37e5a238a4306fb77c4aeff26f
2611Avani <166926965+2611Avani@users.noreply.github.com>
2024-04-18 10:15:17 +0000

    datatypes summary

---
+++ .devcontainer/01_baiscs /datatypesSummary.js
@@ -0,0 +1,27 @@
+const score = 100
+const scoreValue= 100.3
+
+const isLoggedIn = false
+const outTemp = null
+let useremail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+// console.log(id === anotherId)
+
+const bigNumber = 1234567891011121314151617181920n
+
+// Arrays objects n functions 
+
+const heros = ["shaktiman", "nagraj", "doga" ]
+let myobj = {
+    name: "avani",
+    age: 20,
+
+}
+
+const myFunction = function(){
+    //console.log("hello world");
+}
+
+console.log(typeof myFunction);
