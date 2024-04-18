ca46be6597046729bbce92b656d6aeaeb3100c5b
2611Avani <166926965+2611Avani@users.noreply.github.com>
2024-04-18 10:33:47 +0000

    datatypes Summary

--- .devcontainer/01_baiscs /datatypesSummary.js
+++ .devcontainer/01_baiscs /datatypesSummary.js
@@ -24,4 +24,31 @@ const myFunction = function(){
     //console.log("hello world");
 }
 
-console.log(typeof myFunction);
+//console.log(typeof myFunction);
+
+
+//--------------------------------stacks and heap memory in js ---------------------------------------------//
+
+
+let myyoutubename = "avani"
+
+let anothername = myyoutubename
+
+anothername = "coc"
+
+// console.log(myyoutubename);
+// console.log(anothername);
+
+let user1={
+
+    email:"abc@gmail.com",
+
+    upi:"@abc"
+}
+
+let user2 = user1
+
+user2.email= "def@gmail.com"
+
+console.log(user1.email);
+console.log(user2.email);
