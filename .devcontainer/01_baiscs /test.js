a64b96a51970a1a541ba95c0741e056331525fc4
2611Avani <166926965+2611Avani@users.noreply.github.com>
2024-04-18 16:10:27 +0000

    test

--- .devcontainer/01_baiscs /test.js
+++ .devcontainer/01_baiscs /test.js
@@ -35,13 +35,63 @@
 // console.log(c+b);
 
 
+// console.log("1" + 0);
+// console.log( 1 + "0");
+// console.log("1" + 1 + 1);
+// console.log(1 + "0" + 0);
 
+// let marks = 20
+// let newMarks = ++marks and marks++
+// console.log(marks, newMarks);
 
+// console.log("" == 0);
+// comparison operators and stritch check 
 
+// datatype summary 
 
+// const v1 = Symbol('000')
+// const v2 = Symbol('000')
+// console.log(v1===v2);
 
+// arrays
 
+// let coffee = ["espresso",  "icedAmericano", "latte"]
 
+// objects
 
+// let pencil ={
+  //  brand:"apsara",
+  //  strength: "extraDark",
+//}
 
+// functions 
 
+// const myf = function()
+//{
+ //   console.log("hii");
+//}
+
+// console.log(typeof pencil);
+
+// memory stack
+
+let n1 = "csb"
+let n2 = n1
+n2 = "coc"
+// console.log(n1);
+// console.log(n2);
+
+// memory heap 
+
+/*
+
+let us1 = {
+    age: 20
+}
+let us2 =us1
+us2.age = 30
+console.log(us1.age);
+console.log(us2.age);
+
+variable.key can be used to obtain the value stored in the key 
+*/
