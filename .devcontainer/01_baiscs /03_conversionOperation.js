b9714904ed35f95073bcc33b6e71d66f6e071fe2
2611Avani <166926965+2611Avani@users.noreply.github.com>
2024-04-18 05:35:59 +0000

    conversions and operations

---
+++ .devcontainer/01_baiscs /03_conversionOperation.js
@@ -0,0 +1,14 @@
+// let score = "Avani"
+
+// console.log(typeof score);
+// or console.log(typeof (score));
+
+
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+let isLoggedIn = a
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
