12e6d043c502a65233de4093ecd6e9a883e07b40
2611Avani <166926965+2611Avani@users.noreply.github.com>
2024-04-18 08:37:03 +0000

    conversions and operations

--- .devcontainer/01_baiscs /03_conversionOperation.js
+++ .devcontainer/01_baiscs /03_conversionOperation.js
@@ -13,7 +13,47 @@
 // let booleanIsLoggedIn = Boolean (isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
-let someNum = 1
-let stringNum = String (someNum)
-console.log(stringNum);
-console.log(typeof stringNum);
+//let someNum = 1
+//let stringNum = String (someNum)
+//console.log(stringNum);
+//console.log(typeof stringNum);
+
+// ----------------------------operations---------------------------//
+
+let value = 3
+negValue = -value
+// console.log(negValue);
+
+/* console.log(2+2)
+console.log(2-2)
+console.log(2/2)
+console.log(2*2)
+console.log(2%2)
+console.log(2**2) */
+
+let str1 = "hello"
+let str2 = " Avani"
+// console.log(str1 + str2);
+
+/* console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2") */
+
+/* console.log(-true);
+console.log(-false);
+console.log(+true);
+console.log(+false);
+console.log(+"");
+console.log(-""); */
+
+/* let a = 2
+const b = ++a
+console.log("a:" + a ,"b:" + b); */
+
+/* let a = 2
+const b = a++
+console.log("a:" + a ,"b:" + b); */
+
+
+
