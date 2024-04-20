dc922db462e24ec3161e2ea9b0c1ff392a8a0bd5
2611Avani <166926965+2611Avani@users.noreply.github.com>
2024-04-20 11:42:09 +0000

    arrays

---
+++ .devcontainer/02_basics/01_array.js
@@ -0,0 +1,49 @@
+//arrays
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros= ["hulk", "Iron-Man"]
+const myArr2 = new Array (1, 2, 3, 4)
+//console.log(myArr);
+// console.log(myArr[4]);
+
+// --------array methods-------//
+
+// myArr.push(6)
+// myArr.pop()
+
+// myArr.unshift(-1)
+// myArr.shift()
+
+// console.log(myArr.indexOf(5));
+
+const newArr = myArr.join()
+//console.log(typeof newArr);
+// console.log(typeof myArr);
+
+//slice, splice
+
+
+
+// console.log("A:", myArr);
+
+const myn1 = myArr.slice(1, 3)
+const myn2 = myArr.splice()
+
+// console.log("spliced:", myn2);
+
+// console.log("B:", myArr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
