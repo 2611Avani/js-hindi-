46d86d8ce72cbf9b47a4a579e301c3fc991d2cf6
2611Avani <166926965+2611Avani@users.noreply.github.com>
2024-04-20 11:41:57 +0000

    dates n time

--- .devcontainer/01_baiscs /07_datesinJs.js
+++ .devcontainer/01_baiscs /07_datesinJs.js
@@ -7,18 +7,21 @@ let mydate = new Date()
 // let myCreatedDate = new Date(2024, 7, 18)
 // console.log(myCreatedDate.toDateString());
 
-// let myCreatedDate = new Date(2024, 7, 18, 9, 0)
+let myCreatedDate = new Date(2024, 7, 18, 9, 0)
 // console.log(myCreatedDate.toLocaleString());
 
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
 
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getTime());
 
-
-
-
-
-
-
-
+newDate.toLocaleString('default', {
+    weekday: "long"
+})
 
 
 
