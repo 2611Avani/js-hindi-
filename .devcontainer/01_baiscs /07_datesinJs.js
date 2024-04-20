3cda5c21f700656d887522f9ad34f624fe4233b2
2611Avani <166926965+2611Avani@users.noreply.github.com>
2024-04-20 09:35:37 +0000

    dates n time

---
+++ .devcontainer/01_baiscs /07_datesinJs.js
@@ -0,0 +1,24 @@
+// date 
+
+let mydate = new Date()
+// console.log(mydate.toJSON());
+// console.log(typeof mydate);
+
+// let myCreatedDate = new Date(2024, 7, 18)
+// console.log(myCreatedDate.toDateString());
+
+// let myCreatedDate = new Date(2024, 7, 18, 9, 0)
+// console.log(myCreatedDate.toLocaleString());
+
+
+
+
+
+
+
+
+
+
+
+
+
