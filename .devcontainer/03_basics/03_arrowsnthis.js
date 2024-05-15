547d83dad1a0a64dc1473afec4706fbb4a4524df
2611Avani <166926965+2611Avani@users.noreply.github.com>
2024-05-15 04:46:18 +0000

    arrows n this

---
+++ .devcontainer/03_basics/03_arrowsnthis.js
@@ -0,0 +1,52 @@
+/*
+const user = {
+  username: "heaven",
+
+  price: 999,
+
+  welcomeMessage: function () {
+    console.log(`${this.username}, welcome to website`);
+    console.log(this);
+  },
+};
+
+user.welcomeMessage();
+
+user.username = "relish";
+
+user.welcomeMessage();
+
+console.log(this);
+
+*/
+
+/*
+function chai(){
+
+    let username = "heaven"
+    console.log(this.username);
+}
+chai()  */
+
+/*
+const chai = function(){
+    let username = "heaven"
+    console.log(this.username);
+}
+chai()
+*/
+
+/*
+const chai = () => {
+    let username = "heaven"
+    console.log(this);
+    console.log(this.username);
+}
+chai()
+*/ 
+
+const addtwo = (num1, num2) => { 
+
+    return num1 + num2
+}
+console.log(addtwo(3, 4)); 
