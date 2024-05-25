4cf3eeea5eaed1aba9434f09f2194877467693e5
2611Avani <166926965+2611Avani@users.noreply.github.com>
2024-05-25 11:15:11 +0000

    truthy and falsy values

---
+++ .devcontainer/04_controlflow/truthy.js
@@ -0,0 +1,49 @@
+/* const userEmail = "heaven@18.in"
+
+if (userEmail) {
+    console.log("Got the Email");
+}else{
+    console.log("Don't have userEmail");
+}
+*/
+
+// checking array 
+
+/* 
+const power = []
+if(power == 0){
+    console.log("array is empty")
+}
+*/ 
+
+// checking object 
+
+/*
+const emptyObj={}
+
+if (Object.keys(emptyObj).length == 0) {
+    console.log("Object is Empty");
+}
+*/
+
+
+// nullish  coalescing Operator (??)
+
+/*
+let val1
+val1 = 5 ?? 10
+val1 = null ?? 10
+console.log(val1);
+*/
+
+// let val1 = undefined ?? 10
+// console.log(val1);
+
+// ternary operator 
+
+const iceTeaprice = 100
+iceTeaprice <= 80 ? console.log("less than 80") : console.log("more than 80");
+
+
+
+
