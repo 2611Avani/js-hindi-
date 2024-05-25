3d9f4dc922eb308e9bf03ebc4776742d17ff2336
2611Avani <166926965+2611Avani@users.noreply.github.com>
2024-05-25 13:48:10 +0000

    do while loop

---
+++ .devcontainer/05_iterations(loops)/two.js
@@ -0,0 +1,43 @@
+// while loop 
+
+/*
+let i = 0
+while (i <= 10) {
+    console.log(`Value of i is ${i}`);
+    i=i+2
+}
+
+*/
+
+
+/*
+let myArray = ["flash", "hulk", "batman"]
+
+let arr = 0 
+
+while (arr < myArray.length) {
+    
+    console.log(`Value is ${myArray[arr]}`);
+
+    arr++
+}
+*/
+
+// do - while loop 
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
+
+
+
+
+
+
+
+
+
