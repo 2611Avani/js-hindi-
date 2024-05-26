e1e8f20ce22548d3c7d67711f7bd01566ed9fe72
2611Avani <166926965+2611Avani@users.noreply.github.com>
2024-05-26 05:17:08 +0000

    for in loops

---
+++ .devcontainer/05_iterations(loops)/four.js
@@ -0,0 +1,42 @@
+// iterating objects 
+
+/*
+const myObject ={
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    py: 'python',
+
+}
+
+for (const key in myObject) {
+
+    console.log(`shortcut of ${myObject[key]} is ${key}`);
+}
+*/
+
+
+// using for in loop on arrays 
+
+/*
+const programming = ['js', 'cpp', 'rb', 'py', 'dart']
+
+for (const key in programming) {
+ console.log(programming[key]);
+}
+
+*/
+
+
+// map cannot be iterated using for in loops 
+
+/*
+const map = new Map()
+map.set('IN', "India")
+map.set('UK', "United Kingdom")
+map.set('Fr', "France")
+
+for (const key in map) {
+ console.log(key);
+}
+*/
